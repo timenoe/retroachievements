651bb4753942cce9ce8c716dddae2e1113cc8388
Tim Long <timlong105@gmail.com>
2024-12-07 13:13:17 -0500

    Add Nightmare of Druaga alchemy badge generator

---
+++ games/ps2/nightmare_of_druaga/alchemy_badge_generator/create_badges.py
@@ -0,0 +1,96 @@
+"""
+@file create_badges.py
+
+Script to generate badges for the alchemy system in Nightmare of Druaga (PS2)
+"""
+
+import json
+import os
+import typing
+
+from PIL import Image
+
+
+class AlchemyBadgeGenerator:
+    """
+    Class to generate alchemy badges
+    """
+
+    base_image: typing.Any  # mypy being annoying
+    alchemy_json: dict[str, dict[str, dict[str, dict[str, str]]]] = {}
+    layer_json: dict[str, str] = {}
+    combo_counter: dict[str, int] = {}
+
+    def run(self) -> None:
+        """
+        Run all functions to generate badges
+        """
+
+        self.load_files()
+        self.generate_badges()
+
+    def load_files(self) -> None:
+        """
+        Load all necessary files
+        """
+
+        self.base_image = Image.open("layers/base.png").convert("RGBA")
+        os.makedirs("out", exist_ok=True)
+
+        with open("alchemy.json", encoding="utf-8") as f:
+            self.alchemy_json = json.load(f)
+
+        with open("layer.json", encoding="utf-8") as f:
+            self.layer_json = json.load(f)
+
+    def get_paths(self, npc: str, main: str, sub: str) -> list[str]:
+        """
+        Get the layer image paths that represent a combo
+
+        @param npc String NPC name
+        @param main String main item name
+        @param sub String sub item name
+        """
+
+        npc_path = f"layers/{npc.lower()}.png"
+        main_path = f"layers/{self.layer_json[main]}_main.png"
+        sub_path = f"layers/{self.layer_json[sub]}_sub.png"
+        combo = f"{npc_path}{main_path}{sub_path}"
+        if combo not in self.combo_counter:
+            self.combo_counter[combo] = 1
+        else:
+            self.combo_counter[combo] += 1
+        number_path = f"layers/{self.combo_counter[combo]}.png"
+
+        return [npc_path, main_path, sub_path, number_path]
+
+    def generate_badges(self) -> None:
+        """
+        Generate alchemy badges using individual layers
+        """
+
+        combo_index = 1
+
+        # Loop through every item combo in the alchemy JSON file
+        for npc in self.alchemy_json:
+            for main in self.alchemy_json[npc]:
+                for sub in self.alchemy_json[npc][main]:
+
+                    # Get layer image paths
+                    paths = self.get_paths(npc, main, sub)
+
+                    # Overlay all layers onto the base image
+                    new_image = self.base_image
+                    for layer in paths:
+                        overlay_image = Image.open(layer).convert("RGBA")
+                        new_image = Image.alpha_composite(new_image, overlay_image)
+
+                    # Save unique item combo image
+                    new_image.save(
+                        f"out/{combo_index} - {npc} - {main} + {sub}.png", "PNG"
+                    )
+                    combo_index += 1
+
+
+if __name__ == "__main__":
+    AlchemyBadgeGenerator().run()
